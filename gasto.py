a8848dd2a1ab8b518f353b3042f66edd5b45e07b
lucaslimacelino-dev <lucaslimallc123@gmail.com>
2025-07-20 11:02:55 -0300

    Delete gasto.py

--- gasto.py
+++
@@ -1,5 +0,0 @@
-from models.lancamento import Lancamento
-
-class Gasto(Lancamento):
-    def __init__(self, descricao: str, valor: float, categoria: str):
-        super().__init__(descricao, valor, categoria)
