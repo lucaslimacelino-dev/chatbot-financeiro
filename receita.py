54443c8dd3a4ca1b55df064765e2b82860839a27
lucaslimacelino-dev <lucaslimallc123@gmail.com>
2025-07-20 11:03:02 -0300

    Delete receita.py

--- receita.py
+++
@@ -1,5 +0,0 @@
-from models.lancamento import Lancamento
-
-class Receita(Lancamento):
-    def __init__(self, descricao: str, valor: float, categoria: str):
-        super().__init__(descricao, valor, categoria)
